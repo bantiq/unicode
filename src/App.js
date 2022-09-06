7301b02cb34c4c27ede6b35d5c22aa0d0bd3e19c
BantiQ <peremienko.bogdan@gmail.com>
2022-09-07 00:45:58 +0300

    l3t7

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
 
 function App() {
+  const state = {
+    hrefs: [
+      {href: '1.html', text: 'ссылка 1'},
+      {href: '2.html', text: 'ссылка 2'},
+      {href: '3.html', text: 'ссылка 3'},
+    ]
+  };
+  let listState = state.hrefs.map((elem,index) =>
+      <li key = { index }>
+        <a href = {elem.href}>
+          {elem.text}
+        </a>
+      </li>
+  )
   return (
-      <div></div>
+      <div>
+        <ul>
+          {listState}
+        </ul>
+      </div>
   );
 }
 
