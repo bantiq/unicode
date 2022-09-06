2f2b4074411180f67683c57b448cb42d950e838c
BantiQ <peremienko.bogdan@gmail.com>
2022-09-06 23:52:39 +0300

    l2t4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
+import {useState} from "react";
 
 function App() {
+  const [person, setPerson] = useState({
+    name: 'Иван',
+    age: 25
+  });
+  function changePerson(){
+      setPerson({...person , name : 'Коля', age : '30'})
+  }
   return (
-      <div></div>
+      <div>
+        имя: {person.name}, возраст: {person.age}
+        <p/>
+        <button onClick={changePerson}>Натисни на мене</button>
+      </div>
   );
 }
 
