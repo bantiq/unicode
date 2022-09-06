cfa9ca81f6f0bf4c868ba77d34b08bcbd9551681
BantiQ <peremienko.bogdan@gmail.com>
2022-09-07 01:24:04 +0300

    l4t4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  const [arr, setArr] = useState(['Коля', 'Вася', 'Петя', 'Иван', 'Дима']);
+
+    let newElem = () => setArr([...arr,'пункт'])
+    let delElem = (indexDelElem) =>
+        setArr((arr) =>
+            arr.filter((elem,index) => index !== indexDelElem)
+        )
+
+  let listArr = arr.map((elem,index)=>
+      <li key = {index}>
+          {elem}
+          <button onClick={() => delElem(index)}>Натисни кнопку</button>
+      </li>)
+
   return (
-      <div></div>
+      <div>
+        <ul>
+          {listArr}
+        </ul>
+        <button onClick={newElem}>Натисни кнопку</button>
+
+      </div>
   );
 }
 
