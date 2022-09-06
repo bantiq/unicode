acb83655de15ce495ca108e414936973c807a5bf
BantiQ <peremienko.bogdan@gmail.com>
2022-09-06 23:57:39 +0300

    l3t1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 
 function App() {
+  const state  = {
+    show: true,
+    name: 'Иван'
+  };
+  function showMessage(){
+      if (state.show){
+          return <p>Привет, {state.name}!</p>
+      }
+      return <p>Пока, {state.name}</p>
+  }
   return (
-      <div></div>
+      <div>
+          {showMessage()}
+      </div>
   );
 }
 
