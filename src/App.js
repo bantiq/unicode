9457aed440388245ad72ce30ff781c1659964501
BantiQ <peremienko.bogdan@gmail.com>
2022-09-06 23:32:33 +0300

    l1t9

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 
 function App() {
+  let arr = ['a','b','c','d','e'];
+  let listArr = arr.map((elem, index) =>
+      <li key={index}>
+          {elem}
+      </li>);
   return (
-      <div></div>
+      <div>
+        <ul>
+          {listArr}
+        </ul>
+      </div>
   );
 }
 
