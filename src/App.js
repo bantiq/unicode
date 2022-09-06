746fa55b2fa8a046e3ffbc9bdd00cf6e1801d85f
BantiQ <peremienko.bogdan@gmail.com>
2022-09-07 00:34:10 +0300

    l3t2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
+import {useState} from "react";
 
 function App() {
+  const [state, setState] = useState({
+    name: 'Иван',
+    age: 25,
+    show: false
+  });
+  function showMessage() {
+    if (state.show){
+      return <p>имя: {state.name}, возраст: {state.age}</p>
+    }
+    return false;
+  }
+  let changeShow = () => setState({...state, show: true}) ;
   return (
-      <div></div>
+      <div>
+        {showMessage()}
+        <button onClick={changeShow}>Натисни на мене</button>
+      </div>
   );
 }
 
