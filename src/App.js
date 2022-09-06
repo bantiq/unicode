76bb8ec82f0bef673703e127178e2a96cf12f02d
BantiQ <peremienko.bogdan@gmail.com>
2022-09-07 00:48:00 +0300

    l4t1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 function App() {
+    let arr = ['Коля', 'Вася', 'Петя', 'Иван', 'Дима'];
+
+    let listArr = arr.map((elem,index)=> <li key = {index}>{elem}</li>)
   return (
-      <div></div>
+      <div>
+          <ul>
+              {listArr}
+          </ul>
+      </div>
   );
 }
 
