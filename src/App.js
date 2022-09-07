7bd2b05499ea77b182fb5caa77f1a1c789af916d
BantiQ <peremienko.bogdan@gmail.com>
2022-09-07 21:51:48 +0300

    l5t9

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return (
-      <div></div>
-  );
+    const [arr, setArr] = useState(['Олександр','Давид','Максим'])
+    const [inputValue, setInputValue] = useState('')
+    let deleteElem = (indexDeletedElem) => {
+        setArr([...arr.filter((elem,index) => index !== indexDeletedElem)])
+    }
+    let listArr = arr.map((elem,index) =>
+        <li key={index}>
+            {elem}
+            <button onClick={() => deleteElem(index)}>Видалити</button>
+        </li>)
+    let handleChange = (event) => {
+        setInputValue(event.target.value)
+    }
+    let handleClick = () => {
+        if (inputValue) {
+            setArr([...arr, inputValue])
+            setInputValue('')
+        }
+    }
+    return (
+        <div>
+            <input type="text"
+                   onChange={handleChange}
+                   value={inputValue}
+            />
+            <button onClick={handleClick}>Натисни</button>
+            <ul>{listArr}</ul>
+        </div>
+    );
+
 }
 
 export default App;
