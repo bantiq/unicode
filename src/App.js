e04c7dbe5d8ac89cc4051efa8101e5aedc15262b
BantiQ <peremienko.bogdan@gmail.com>
2022-09-07 21:44:22 +0300

    l5t4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  const [str, setStr] = useState('')
+  let arrSplit = str.split(' ')
   return (
-      <div></div>
+      <div>
+        <input type="text" value={str}
+               onChange={(e) => setStr(e.target.value)}
+        />
+        <p>{arrSplit[0]}</p>
+        <p>{arrSplit[1]}</p>
+        <p>{arrSplit[2]}</p>
+      </div>
   );
 }
 
