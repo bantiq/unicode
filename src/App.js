d24ff7f1f06afc8fea621f35082b9124e10e9190
BantiQ <peremienko.bogdan@gmail.com>
2022-09-07 21:52:25 +0300

    l5t10

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return (
-      <div></div>
-  );
+    const [state, setState] = useState({
+        hrefs: [
+            {href: '1.html', text: 'Посилання 1'},
+            {href: '2.html', text: 'Посилання 2'},
+            {href: '3.html', text: 'Посилання 3'},
+        ]
+    });
+    const [value, setValue] = useState({
+        linkValue : '',
+        textValue : '',
+    })
+    let listState = state.hrefs.map((elem,index) =>
+        <li key={index}>
+            <a href={elem.href}>{elem.text}</a>
+        </li>
+    )
+    let changeLinkValue = (event) => setValue({...value,linkValue: event.target.value})
+    let changeTextValue = (event) => setValue({...value,textValue: event.target.value})
+    let handleClick = () => {
+        setState({...state, ...state.hrefs.push({
+                href: value.linkValue,
+                text: value.textValue
+            })
+        })
+        setValue({
+            ...value,
+            linkValue : '',
+            textValue : ''
+        })
+    }
+    return (
+        <div>
+            <ul>
+                {listState}
+            </ul>
+            <input type="text"
+                   placeholder='Посилання'
+                   value={value.linkValue}
+                   onChange={changeLinkValue}
+            />
+            <input type="text"
+                   placeholder='Текст'
+                   value={value.textValue}
+                   onChange={changeTextValue}
+            />
+            <button onClick={handleClick}>Додати</button>
+        </div>
+    )
+
+
 }
 
 export default App;
