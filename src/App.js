31a0e9baf044fd935eb913de8a72bc993d284d50
BantiQ <peremienko.bogdan@gmail.com>
2022-09-07 21:43:31 +0300

    l5t3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return (
-      <div></div>
-  );
+    const [value, setValue] = useState('')
+
+    let birthdayYear = new Date().getFullYear();
+    let changeValue = () => value ?
+        birthdayYear - value :
+        false;
+
+    return (
+        <div>
+            <input type="number" value={value}
+                   onChange={(event) => setValue(event.target.value)}
+            />
+            <p>{changeValue()}</p>
+        </div>
+    );
 }
 
 export default App;
