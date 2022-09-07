17dfab9c152087424d77ded1fcd8e5b21f949c1d
BantiQ <peremienko.bogdan@gmail.com>
2022-09-07 21:51:01 +0300

    l5t8

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return (
-      <div></div>
-  );
+    const [arr, setArr] = useState(['Олександр','Давид','Максим'])
+    const [inputValue, setInputValue] = useState('')
+    let listArr = arr.map((elem,index) => <li key={index}>{elem}</li>)
+    let handleChange = (event) => {
+        setInputValue(event.target.value)
+    }
+    let handleClick = () => {
+        setArr([...arr, inputValue])
+        setInputValue('')
+    }
+    return (
+        <div>
+            <input type="text"
+                   onChange={handleChange}
+                   value={inputValue}
+            />
+            <button onClick={handleClick}>Натисни</button>
+            <ul>{listArr}</ul>
+        </div>
+    );
+
 }
 
 export default App;
