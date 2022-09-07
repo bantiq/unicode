fdfc944c447082d271cca6c0e6fcbbf9f43b129e
BantiQ <peremienko.bogdan@gmail.com>
2022-09-07 21:46:25 +0300

    l5t5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return (
-      <div></div>
-  );
+    const [value, setValue] = useState({
+        inputValue : '',
+        returnValue : ''
+    })
+    let handleChange = (event) =>
+        setValue({
+            ...value
+            ,inputValue : event.target.value
+        })
+    ;
+    let handleClick = () => {
+        setValue({
+            ...value,
+            returnValue : value.inputValue,
+            inputValue : ''})
+        console.log(value)
+    }
+
+    return (
+        <div>
+            <input type="text"
+                   onChange={handleChange}
+                   value={value.inputValue}
+            />
+            <button onClick={handleClick}>Submit</button>
+            <p>{value.returnValue}</p>
+        </div>
+    );
 }
 
 export default App;
